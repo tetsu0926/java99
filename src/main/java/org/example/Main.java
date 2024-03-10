d4910035a8a6a79c1d7006df05487f4793ce53a5
totoro <tetsu0926@gmail.com>
2024-03-10 21:58:09 +0900

    vscode 4 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("VSCODE IDEA");
         System.out.println("IntelliJ IDEA");
+
+        System.out.println("vscode test...");
     }
 }
