259765501db0541a716c616ed290ecea9c2a7a29
totoro <tetsu0926@gmail.com>
2024-03-10 21:47:51 +0900

    커밋

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("vscode!");
         System.out.println("이건 충돌..");
+
+        System.out.println("이건 vscode에서 수정..");
     }
 }
