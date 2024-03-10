c10691b69e4f3b0b12ada5d1afa4f496368e1d11
totoro <tetsu0926@gmail.com>
2024-03-10 22:12:27 +0900

    vscode 6 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("브이에스 코드.");
         System.out.println("인텔리제이 코드.");
+        System.out.println("정리.");
     }
 }
