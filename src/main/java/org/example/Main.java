f6d96cd918715dbfae6b61434fbb4cf7a2b5fd29
totoro <tetsu0926@gmail.com>
2024-03-10 21:46:48 +0900

    인텔리제이 1미2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("vscode!");
         System.out.println("이건 충돌..");
+        System.out.println("인텔리제이 커밋");
     }
 }
