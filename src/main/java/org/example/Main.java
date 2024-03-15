1116f825b50d028833687565894da2594915a8a3
totoro <tetsu0926@gmail.com>
2024-03-15 21:35:49 +0900

    인텔리제이 충돌

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("브이에스 코드_1.");
         System.out.println("인텔리제이 코드_2.");
         System.out.println("source tree commit....");
+        System.out.println("인텔리제이 충돌...");
     }
 }
