1f7de94b3d47f07c3ed328246313e65bf9aa0294
totoro <tetsu0926@gmail.com>
2024-03-26 22:11:26 +0900

    temp

--- src/main/java/org/example/T10.java
+++ src/main/java/org/example/T10.java
@@ -3,7 +3,9 @@ package org.example;
 public class T10 {
 
     public static void main(String[] args) {
-
-        System.out.println("123456789");
+        System.out.println("테스트");
+        System.out.println("잘되는건가??");
+        System.out.println("후...");
+        System.out.println("후...");
     }
 }
