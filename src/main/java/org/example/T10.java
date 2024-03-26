7a6379b21e69a90da88f9cdf1eaff59469f0c0f4
totoro <tetsu0926@gmail.com>
2024-03-26 22:19:05 +0900

    이렇게 해야하나?

--- src/main/java/org/example/T10.java
+++ src/main/java/org/example/T10.java
@@ -5,5 +5,6 @@ public class T10 {
     public static void main(String[] args) {
         System.out.println("테스트");
         System.out.println("테스트99");
+        System.out.println("테스트1");
     }
 }
