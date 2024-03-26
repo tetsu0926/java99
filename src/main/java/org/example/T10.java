0a896841e79abc463d8ae334cf807a27a7cdcc95
totoro <tetsu0926@gmail.com>
2024-03-26 22:15:44 +0900

    일단 커밋

--- src/main/java/org/example/T10.java
+++ src/main/java/org/example/T10.java
@@ -4,8 +4,5 @@ public class T10 {
 
     public static void main(String[] args) {
         System.out.println("테스트");
-        System.out.println("잘되는건가??");
-        System.out.println("후...");
-        System.out.println("병합이되는건가?");
     }
 }
