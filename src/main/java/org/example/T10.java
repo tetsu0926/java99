25d75a0db49a1065ee585ff99debf7f90b1e0968
totoro <tetsu0926@gmail.com>
2024-03-29 23:32:29 +0900

    master 1

--- src/main/java/org/example/T10.java
+++ src/main/java/org/example/T10.java
@@ -6,5 +6,6 @@ public class T10 {
         System.out.println("my first java code");
         System.out.println("my first java code 1");
         System.out.println("my first java code 2");
+        System.out.println("my first java code 3");
     }
 }
