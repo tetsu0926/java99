89ebe8d5079416e583e035e0beef5c4cfd590fae
totoro <tetsu0926@gmail.com>
2024-03-29 23:23:14 +0900

    my 1

--- src/main/java/org/example/T10.java
+++ src/main/java/org/example/T10.java
@@ -3,8 +3,8 @@ package org.example;
 public class T10 {
 
     public static void main(String[] args) {
-        System.out.println("테스트");
-        System.out.println("테스트99");
-        System.out.println("테스트1");
+        System.out.println("my first java code");
+        System.out.println("my first java code 1");
+        System.out.println("my first java code 2");
     }
 }
