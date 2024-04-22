d2848e1ae2db98afb5f2e50bcde0d3c4989ff409
totoro <tetsu0926@gmail.com>
2024-04-22 21:55:00 +0900

    test

--- src/main/java/org/example/T10.java
+++ src/main/java/org/example/T10.java
@@ -4,7 +4,7 @@ public class T10 {
 
     public static void main(String[] args) {
         System.out.println("my first java code");
-        System.out.println("feature 1");
-        System.out.println("feature 2");
+        System.out.println("test 1");
+        System.out.println("test 2");
     }
 }
