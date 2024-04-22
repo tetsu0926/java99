c0a3f8192c9b89cde571bc74fc6c037013953f1e
totoro <tetsu0926@gmail.com>
2024-04-22 21:55:00 +0900

    feature 1

--- src/main/java/org/example/T10.java
+++ src/main/java/org/example/T10.java
@@ -4,10 +4,6 @@ public class T10 {
 
     public static void main(String[] args) {
         System.out.println("my first java code");
-        System.out.println("my first java code 1");
-        System.out.println("my first java code 2");
-        System.out.println("my first java code 3");
-        System.out.println("my first java code 40");
-        System.out.println("my first java code 41");
+        System.out.println("feature 1");
     }
 }
