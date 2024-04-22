fe2ff1c7bec68c5e15bfd19cd76503496cff60cf
totoro <tetsu0926@gmail.com>
2024-04-22 22:01:15 +0900

    lotto

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,24 +9,28 @@ public class Main {
 
         TreeSet<Integer> lotto = new TreeSet<>();
 
-        int total = 100;
+//        int total = 180;
+        int count = 20240420;
         List<Integer> list = Arrays.asList(3, 13, 30, 33, 43, 45);
 
-        for(int i=0; i<1005; i++) {
-            lotto.clear();
-            while (lotto.size() < 6) {
-                int number = new Random().nextInt(45) + 1;
-                boolean flag = list.stream().anyMatch(s -> s == number);
-                if(!flag) lotto.add(number);
-            }
-            int sum = lotto.stream().mapToInt(Integer::intValue).sum();
-            if(sum >= total) {
-                lotto.clear();
+        int sum = 0;
+        int no = 0;
+        while (lotto.size() < 6) {
+            int number = new Random().nextInt(45) + 1;
+            boolean flag = list.stream().anyMatch(s -> s == number);
+            if (!flag) lotto.add(number);
+            if(lotto.size()== 6 ){
+                sum = lotto.stream().mapToInt(Integer::intValue).sum();
+                if (no < count) {
+                    lotto.clear();
+                }
+                no++;
             }
         }
 
 
-        int sum = lotto.stream().mapToInt(Integer::intValue).sum();
+
+            sum = lotto.stream().mapToInt(Integer::intValue).sum();
         System.out.println(sum);
         System.out.println(lotto.toString());
 
